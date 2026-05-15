d09239d2a9a14913041fb54cdfdb36a1c90b6d80
Mothusi89 <2025592@student.cct.ie>
2026-05-15 14:38:52 +0100

    Initial commit

---
+++ src/main/java/com/mycompany/javaprojca_2/JAVAprojCA_2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.javaprojca_2;
+
+/**
+ *
+ * @author gondo
+ */
+public class JAVAprojCA_2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
